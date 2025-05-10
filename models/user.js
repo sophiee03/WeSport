29b5502329e2269cfad2f85d42d0f3a13662e34a
evelin.begher <evelin.begher@studenti.unitn.it>
2025-05-10 17:39:47 +0200

    Aggiunto file  per gestione registrazione utente

---
+++ models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('User', userSchema);
+
+secret2FA: { type: String }, // segreto per Google Authenticator
+is2FAEnabled: { type: Boolean, default: false }
