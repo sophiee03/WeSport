b81cfb6dbd760d94e36e0bf6e4f88ebd8538a0c3
Sophie Motter <sophiemotter.03@gmail.com>
2025-05-14 11:34:11 +0200

    integrato searchareasportiva nel file areasportiva

--- paths/areasportiva.js
+++ paths/areasportiva.js
@@ -136,4 +136,33 @@ router.post('/votoareesportive/:id', verifyToken, async (req, res) => {
   }
 });
 
+/** COMMENTI SWAGGER
+ * @openapi
+ * /areasportiva/searchAS
+ *       description: Ottieni le aree sportive filtrate per categoria
+ *       parameters:
+ *       - name: type
+ *           in: query
+ *           description: Categoria dell'area sportiva (ad esempio: calcio, tennis, basket)
+ *           required: true
+ *           schema:
+ *           type: string
+ *       responses:
+ *       '200':
+ *           description: Successo - aree sportive filtrate per categoria
+ *       '400':
+ *           description: Parametro 'type' mancante
+ *       '500':
+ *           description: Errore interno
+ */
+
+router.get('/search', async (req, res) => {
+  try {
+    const searchAS = await areasportiva.find();
+    res.json(searchAS); 
+  } catch (err) {
+    res.status(500).json({ errore: 'Errore nel recupero dei dati' });
+  }
+});
+
 module.exports = router;
