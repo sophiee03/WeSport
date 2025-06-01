367f1cfee5df3ee1d7832106f6209246d8f79d0c
Sophie Motter <sophiemotter.03@gmail.com>
2025-06-01 16:12:55 +0200

    visualizzazione e lista avvisi

--- frontend/navigation/appNavigation.js
+++ frontend/navigation/appNavigation.js
@@ -3,16 +3,19 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 // importa i file JS
 import visualizzazioneAS from '../screens/visualizzazioneAS'; 
+import LoginUI from '../screens/LoginUI';
 import HomePage from '../screens/HomePage';
 import ProfiloUtente from '../screens/ProfiloUtente';
 import ListaAnnunci from '../screens/ListaAnnunci';
-import visualizzaAnnuncio from '../screens/visualizzaAnnuncio';
+import visualizzazioneAnnuncio from '../screens/visualizzazioneAnnuncio';
 import creazioneAnnuncio from '../screens/creazioneAnnuncio';
 import chat from '../screens/chat';
-import Avvisi from '../screens/Avvisi';
+import ListaAvvisi from '../screens/ListaAvvisi';
 import ListaPercorsi from '../screens/ListaPercorsi';
 import VisualizzazionePercorso from '../screens/visualizzazionePercorso';
 import CreazionePercorso from '../screens/creazionePercorso';
+import visualizzazioneAvviso from '../screens/visualizzazioneAvviso';
+
 
 const Stack = createNativeStackNavigator();
 
@@ -20,13 +23,15 @@ const AppNavigation = () => {
   return (
     <Stack.Navigator initialRouteName="HomePage">
       <Stack.Screen name="Home" component={HomePage} />
+      <Stack.Screen name="LoginUI" component={LoginUI} />
       <Stack.Screen name="AreeSportive" component={visualizzazioneAS} />
       <Stack.Screen name="ProfiloUtente" component={ProfiloUtente} />
       <Stack.Screen name="ListaAnnunci" component={ListaAnnunci} />
-      <Stack.Screen name="visualizzaAnnuncio" component={visualizzaAnnuncio} />
+      <Stack.Screen name="visualizzazioneAnnuncio" component={visualizzazioneAnnuncio} />
       <Stack.Screen name="creazioneAnnuncio" component={creazioneAnnuncio} />
       <Stack.Screen name="chat" component={chat} />
-      <Stack.Screen name="Avvisi" component={Avvisi} />
+      <Stack.Screen name="ListaAvvisi" component={ListaAvvisi} />
+      <Stack.Screen name="visualizzazioneAvviso" component={visualizzazioneAvviso} />
       <Stack.Screen name="ListaPercorsi" component={ListaPercorsi} />
       <Stack.Screen name="VisualizzazionePercorso" component={VisualizzazionePercorso} /> 
       <Stack.Screen name="creazionePercorso" component={CreazionePercorso} /> 
