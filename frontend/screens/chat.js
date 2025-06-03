e7c393eb268196b734c36cf8ce310d1a491dd675
evelin.begher <evelin.begher@studenti.unitn.it>
2025-06-03 11:45:10 +0200

    modifiche

--- frontend/screens/chat.js
+++ frontend/screens/chat.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import {View, Text, TextInput, FlatList, TouchableOpacity, StyleSheet, KeyboardAvoidingView, Platform, Keyboard, SafeAreaView} from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
-import { getHeaders, getnomeutente } from '../utils/apiUtils';
+import { getHeaders, getnomeutente } from '../utils/apiutils';
 import BarraSezioni from '../components/barraSezioni';
 import Messaggio from '../components/messaggio.js';
 import Ionicons from 'react-native-vector-icons/Ionicons';
